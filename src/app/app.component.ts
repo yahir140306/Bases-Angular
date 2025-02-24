1a43a92ac21b08983e05872e253354a74e0c5dd5
yahir140306 <joseyahirhernandez11@gmail.com>
2025-02-23 22:13:45 -0600

    Agregar componente de contador y actualizar título de la aplicación

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'bases';
+  public title: string = 'Mi Primera App en Angular';
+  public counter: number = 10;
+
+  increaseBy(value: number): void {
+    // if (value === 0) { this.counter = 10; }
+    this.counter += value;
+  }
+  // decreaseBy(): void {
+  //   this.counter -= 1;
+  // }
+  resetCounter() {
+    this.counter = 10;
+  }
 }
---
+++ src/app/counter/counter.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  template: '<h1>Counter Component</h1>',
+  // templateUrl: './counter.component.html',
+  // styleUrls: ['./counter.component.css'],
+})
+export class CounterComponent {}
